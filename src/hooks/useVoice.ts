ef23663463a86189283644d39f95f75de3686e47
JJW <stickbox@naver.com>
2025-07-31 16:22:23 +0900

    voice speed 1.3

--- src/hooks/useVoice.ts
+++ src/hooks/useVoice.ts
@@ -123,7 +123,7 @@ export function useVoice() {
     }
     const utter = new window.SpeechSynthesisUtterance(text);
     utter.lang = "ko-KR";
-    utter.rate = 1.1; // 1.1배속 설정
+    utter.rate = 1.3; // 1.1배속 설정
     if (onEnd) {
       utter.onend = onEnd;
     }
