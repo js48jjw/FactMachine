93e21e0ee29e68b08fc4ce057913494b562e4ee9
JJW <stickbox@naver.com>
2025-07-31 15:17:29 +0900

    headtitle 1 call

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -59,7 +59,6 @@ const ChatWindow: React.FC<ChatWindowProps> = ({
 
   return (
     <>  
-    <TitleHeader />     
       <div className="flex-1 overflow-y-auto p-4">
         {messages.length === 0 ? (
           <div className="flex flex-1 min-h-[40vh] items-center justify-center">
