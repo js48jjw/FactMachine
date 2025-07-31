f6b6ba94eb863751872c21cfffa658c5959ea517
JJW <stickbox@naver.com>
2025-07-31 16:19:46 +0900

    build error clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import TitleHeader from "../components/TitleHeader";
 import ChatWindow from "../components/ChatWindow";
 import { useVoice } from "../hooks/useVoice";
